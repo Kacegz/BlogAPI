ecc9a98b425bdc158a7c7f9235356ef59d7abac0
Kacegz <90635502+Kacegz@users.noreply.github.com>
2024-05-22 17:15:07 +0200

    setup routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const postController = require("../controllers/postController");
+const commentController = require("../controllers/commentController");
+const userController = require("../controllers/userController");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
+router.get("/posts", postController.list);
+router.get("/posts/:postid", postController.detail);
+router.post("/posts", postController.create);
+router.delete("/posts/:postid", postController.delete);
+
+router.get("/posts/:postid/comments", commentController.list);
+router.post("/posts/:postid/comments", commentController.create);
+router.delete("/posts/:postid/comments/:commentid", commentController.delete);
+/*
+router.get("/posts", commentController.list);
+router.get("/posts/:id", commentController.detail);
+router.post("/posts", commentController.create);
+router.delete("/posts/:id", commentController.delete);
+*/
+router.post("/register", userController.register);
+router.post("/log_in", userController.log_in);
 
 module.exports = router;
