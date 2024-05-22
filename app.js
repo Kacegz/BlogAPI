80e344ec5ac8f6a430670c1eb526e5508dbd9c00
Kacegz <90635502+Kacegz@users.noreply.github.com>
2024-05-22 17:16:28 +0200

    db config

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const mongodb = process.env.db;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongodb);
+}
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 
 const app = express();
 
@@ -15,12 +23,11 @@ app.set("view engine", "ejs");
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
